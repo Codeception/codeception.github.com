f5a45dba4e6344f1389c16831ed85dd76c3c7d3c
Davert <davert.php@resend.cc>
2014-02-13 06:39:32 +0200

    updated post

---
+++ RoboFile.php
@@ -0,0 +1,30 @@
+<?php
+class Robofile
+{
+    use Robo\Output;
+    use Robo\Task\FileSystem;
+    use Robo\Task\Git;
+ 
+ 	function post()
+ 	{
+ 		$title = $this->ask("Post title");
+ 		$file = strtolower(str_replace([' ',':'], ['-','-'], $title));
+ 		$date = date('Y-m-d');
+ 		$this->taskWriteToFile("_posts/$date-$file.markdown")
+ 			->line('---')
+ 			->line('layout: post')
+ 			->line("title: $title")
+ 			->line("date: $date 01:03:50")
+ 			->line("---\n")
+ 			->run();
+ 	}
+
+ 	function publish()
+ 	{
+ 		$this->taskGit()
+ 			->add('-A')
+ 			->commit('updated post')
+ 			->push()
+ 			->run();
+ 	}
+}
