dc32aeb80838276a5b4192156bb1d88f22fc6775
Davert <davert.php@resend.cc>
2017-11-09 02:27:48 +0200

    updated

--- _plugins/search.rb
+++ _plugins/search.rb
@@ -1,15 +1,15 @@
 class AlgoliaSearchJekyllPush < Jekyll::Command
   class << self
     # Hook to exclude some files from indexing
-    def custom_hook_excluded_file?(file)
-      return true if file.path =~ %r{^/_site/}
-      return true if file.path =~ %r{^/_docs-1.8/}
-      return true if file.path =~ %r{^/_docs-2.0/}
-      return true if file.path =~ %r{^/_slides/}
-      return true if file.basename =~ %r{^2012-}
-      return true if file.basename =~ %r{^2013-}
-      return true if file.basename =~ %r{^2014-}
-      false
-    end
+    # def custom_hook_excluded_file?(file)
+    #   return true if file.path =~ %r{^/_site/}
+    #   return true if file.path =~ %r{^/_docs-1\.8/}
+    #   return true if file.path =~ %r{^/_docs-2\.0/}
+    #   return true if file.path =~ %r{^/_slides/}
+    #   return true if file.basename =~ %r{^2012-}
+    #   return true if file.basename =~ %r{^2013-}
+    #   return true if file.basename =~ %r{^2014-}
+    #   false
+    # end
   end
 end
